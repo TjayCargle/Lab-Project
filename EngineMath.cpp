3173f2c972bca74b0fbfc63d56cfd7d2ad73adff
TJay Cargle <tjcargle21@gmail.com>
2016-06-30 16:24:48 -0400

    initial

---
+++ EngineMath.cpp
@@ -0,0 +1,564 @@
+/**
+* @file EngineMath.cpp
+*
+*/
+
+#include "EngineMath.h"
+
+//////////////////////////////////////////////////////////////////////////
+// Common math functions
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+// General Utility functions
+//////////////////////////////////////////////////////////////////////////
+
+// Are two floating point numbers equal to each other
+// Floating Point Error Safe
+//
+// IN:		a		The first number
+//			b		The second number
+//
+// RETURN: TRUE iff |a-b| < Tolerance
+//
+// NOTE:	EPSILON is tolerance
+bool IsEqual(float a, float b)
+{
+	// NOTE: Do not modify.
+	return fabs(a - b) < EPSILON;
+}
+
+// Is a floating point value equal to zero
+// Floating Point Error Safe
+//
+// IN:		a		The number to check
+//
+// RETURN:	TRUE iff |a| < Tolerance
+//
+// NOTE:	Tolerance set by EPSILON
+bool IsZero(float a)
+{
+	// NOTE: Do not modify
+	return (fabs(a))<EPSILON;
+}
+
+// RETURN: MAX of two numbers
+float Max(float a, float b)
+{
+	// NOTE: Do not modify.
+	return (a > b) ? a : b;
+}
+
+// RETURN: MIN of two numbers
+float Min(float a, float b)
+{
+	// NOTE: Do not modify.
+	return (a < b) ? a : b;
+}
+
+// RETURN: Converts input to radian measure
+float Degrees_To_Radians(float Deg)
+{
+	// NOTE: Do not modify.
+	return Deg * PI / 180.0f;
+}
+
+// RETURN: Converts input to degree measure
+float Radians_To_Degrees(float Rad)
+{
+	// NOTE: Do not modify.
+	return Rad * 180.0f / PI;
+}
+////////////////////////////////////////////////////////////////////////
+// Linear Algebra Functions Day 1
+///////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+// Vector Functions
+//////////////////////////////////////////////////////////////////////////
+
+// Check if two TVECTOR's are equal to each other
+//
+// IN:		v		First Vector
+//			w		Second Vector
+//
+// RETURN:  True if v==w, False otherwise
+//
+// NOTE:	Use's all four components
+//			Should be floating point error safe.
+bool Vector_IsEqual(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return false;
+}
+
+// ADD two TVECTOR's togother
+//
+// IN:		v		First Vector. Left Hand Side
+//			w		Second Vector. Right Hand Side
+//
+// RETURN:  v + w
+//
+// NOTE:	Use's all four components
+TVECTOR Vector_Add(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	
+	
+	return v;
+}
+
+// SUBTRACT one TVECTOR from another
+//
+// IN:		v		First Vector. Left Hand Side
+//			w		Second Vector. Right Hand Side
+//
+// RETURN:  v - w
+//
+// NOTE:	Use's all four components
+TVECTOR Vector_Sub(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+// MULTIPLY all four components of a TVECTOR by a scalar
+//
+// IN:		v		The vector to scale
+//			s		The value to scale by
+//
+// RETURN:  s * v
+TVECTOR Vector_Scalar_Multiply(TVECTOR v, float s)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+// NEGATE all the components of a TVECTOR
+//
+// IN:		v		The vector to negate
+//
+// RETURN:	-1 * v
+//
+// NOTE:	Use's all four components
+TVECTOR Vector_Negate(TVECTOR v)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+// Perform a Dot Product on two TVECTOR's
+//
+// IN:		v		First Vector. Left Hand Side
+//			w		Second Vector. Right Hand Side
+//
+// RETURN:  v (DOT) w
+//
+// NOTE:	Use's all four components
+float Vector_Dot(TVECTOR v, TVECTOR w)
+{
+	  
+	// TODO LAB 1: Replace with your implementation.
+	return  1.0;
+}
+
+// Perform a Cross Product on two TVECTOR's
+//
+// IN:		v		First Vector. Left Hand Side
+//			w		Second Vector. Right Hand Side
+//
+// RETURN:  v (CROSS) w
+//
+// NOTE:	The w-component of each vector is not used.
+//			The resultant vector will have a w-component of zero.
+TVECTOR Vector_Cross(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+// Find the squared length of a TVECTOR
+//
+// IN:		v		The vector to find the squared length of
+//
+// RETURN:	Squared Length of TVECTOR
+//
+// NOTE:	Use's all four components
+float Vector_LengthSq(TVECTOR v)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return 0;
+}
+
+// Find the length of a TVECTOR
+//
+// IN:		v		The vector to find the length of
+//
+// RETURN:	Length of TVECTOR
+//
+// NOTE:	Use's all four components
+float Vector_Length(TVECTOR v)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return 0;
+}
+
+// Normalize a TVECTOR
+//
+// IN:		v		The vector to normalize
+//
+// RETURN:	Normalized version of v
+//
+// NOTE:	Use's all four components
+TVECTOR Vector_Normalize(TVECTOR v)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+// Makes a TVECTOR's w-component normalized
+//
+// IN:		v		The vector (point object) to homogenise
+//
+// RETURN:	The homogenised vector (point)
+//
+// NOTE:	If the w-component of the vector is 0 then the
+//			function will return a zero vector with a w-component
+//			of 0.
+TVECTOR Vector_Homogenise(TVECTOR v)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+// Get a TVECTOR made from the maximun components of two TVECTORs
+//
+// IN:		v		The first vector
+//			w		The second vector
+//
+// RETURN:	A maximized vector
+//
+// NOTE:	Use's all four components
+TVECTOR Vector_Maximize(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+// Get a TVECTOR made from the minimum components of two TVECTOR's
+//
+// IN:		v		The first vector
+//			w		The second vector
+//
+// RETURN:	A minimum vector
+//
+// NOTE:	Use's all four components
+TVECTOR Vector_Minimize(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+// Get a TVECTOR made from the average of two TVECTORs
+//
+// IN:		v		The first vector
+//			w		The second vector
+//
+// RETURN:	A vector made from the average of two vectors
+//
+// NOTE:	Use's all four components
+
+TVECTOR Vector_Average(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+// Find the angle between two TVECTORs
+//
+// IN:		v		The first vector
+//			w		The second vector
+//
+// RETURN:  The angle in degrees between the two vectors
+//
+// NOTE:	If either vector is a zero vector then the return
+//			value will be 0.
+float Vector_AngleBetween(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return 0;
+}
+
+// Get the distance one TVECTOR points in the direction of another
+// TVECTOR
+//
+// IN:		v		The first vector
+//			w		The direction of the component
+//
+// RETURN:	The distance that v points in the direction of w.
+//
+// NOTE:	If w or v is a zero vector then the return value is zero.
+float Vector_Component(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return 0;
+}
+
+// Get the TVECTOR that represents v projected on w.
+//
+// IN:		v		The first vector
+//			w		The direction of the projection
+//
+// RETURN:	The projection of v onto w
+//
+// NOTE:	If w or v is a zero vector then the return value is zero.
+TVECTOR Vector_Project(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 1: Replace with your implementation.
+	return v;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Functions Lab  #2
+///////////////////////////////////////////////////////////////////////
+
+
+// Get the reflection of v across w
+//
+// IN:		v		The vector to reflect
+//			w		The "axis" to reflect across
+//
+// RETURN:	v reflected across w
+//
+// NOTE:	If w is a zero vector then return -v.
+TVECTOR Vector_Reflect(TVECTOR v, TVECTOR w)
+{
+	// TODO LAB 2: Replace with your implementation.
+	return v;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Matrix Functions
+//////////////////////////////////////////////////////////////////////////
+
+// Get a [0] matrix
+//
+// RETURN: A 0 4x4 matrix
+TMATRIX Matrix_Zero(void)
+{
+	// TODO LAB 2: Replace with your implementation.
+	TMATRIX m;
+	m._e11 = 11;
+	m._e14 = 14;
+	m._e23 = 23;
+
+	return m;
+}
+
+// Get a [I] matrix
+//
+// RETURN: A 4x4 Identity matrix
+TMATRIX Matrix_Identity(void)
+{
+	// TODO LAB 2: Replace with your implementation.
+	TMATRIX m = { 1, };
+	return m;
+}
+
+// Get a translation matrix
+//
+// IN:		x		Amount of translation in the x direction
+//			y		Amount of translation in the y direction
+//			z		Amount of translation in the z direction
+//
+// RETURN:	The translation matrix
+TMATRIX Matrix_Create_Translation(float x, float y, float z)
+{
+	// TODO LAB 2: Replace with your implementation.
+	TMATRIX m = { 1, };
+	return m;
+}
+
+// Create a scale matrix
+//
+// IN:		x		Amount to scale in the x direction
+//			y		Amount to scale in the y direction
+//			z		Amount to scale in the z direction
+//
+// RETURN:	The scale matrix
+TMATRIX Matrix_Create_Scale(float x, float y, float z)
+{
+	// TODO LAB 2: Replace with your implementation.
+	TMATRIX m = { 1, };
+	return m;
+}
+
+// Get a rotation matrix for rotation about the x-axis
+//
+// IN:		Deg		Angle to rotate ( Degree measure)
+//
+// RETURN:	A X-Rotation Matrix
+TMATRIX Matrix_Create_Rotation_X(float Deg)
+{
+	// TODO LAB 2: Replace with your implementation.
+	TMATRIX m = { 1, };
+	return m;
+}
+
+// Get a rotation matrix for rotation about the y-axis
+//
+// IN:		Deg		Angle to rotate ( Degree measure)
+//
+// RETURN:	A Y-Rotation Matrix
+TMATRIX Matrix_Create_Rotation_Y(float Deg)
+{
+	// TODO LAB 2: Replace with your implementation.
+	TMATRIX m = { 1, };
+	return m;
+}
+
+// Get a rotation matrix for rotation about the z-axis
+//
+// IN:		Deg		Angle to rotate ( Degree measure)
+//
+// RETURN:	A Z-Rotation Matrix
+TMATRIX Matrix_Create_Rotation_Z(float Deg)
+{
+	// TODO LAB 2: Replace with your implementation.
+	TMATRIX m = { 1, };
+	return m;
+}
+
+// ADD two matrices together
+//
+// IN:		m		The first matrix
+//			n		The second matrix
+//
+// RETURN: m + n
+TMATRIX Matrix_Matrix_Add(TMATRIX m, TMATRIX n)
+{
+	// TODO LAB 2: Replace with your implementation.
+	return m;
+}
+
+// SUBTRACT two matrices
+//
+// IN:		m		The first matrix (left hand side)
+//			n		The second matrix (right hand side)
+//
+// RETURN: m - n
+TMATRIX Matrix_Matrix_Sub(TMATRIX m, TMATRIX n)
+{
+	// TODO LAB 2: Replace with your implementation.
+	return m;
+}
+
+// Multiply a matrix by a scalar
+//
+// IN:		m		The matrix to be scaled (right hand side)
+//			s		The value to scale by   (left hand side)
+//
+// RETURN:	The matrix formed by s*[m]
+TMATRIX Matrix_Scalar_Multiply(TMATRIX m, float s)
+{
+	// TODO LAB 2: Replace with your implementation.
+	return m;
+}
+
+// Negate a matrix
+//
+// IN:		m		The matrix to negate
+//
+// RETURN:  The negation of m
+TMATRIX Matrix_Negate(TMATRIX m)
+{
+	// TODO LAB 2: Replace with your implementation.
+	return m;
+}
+
+// Transpose a matrix
+//
+// IN:		m		The matrix to transpose
+//
+// RETURN:	The transpose of m
+TMATRIX Matrix_Transpose(TMATRIX m)
+{
+	// TODO LAB 2: Replace with your implementation.
+	return m;
+}
+
+// Multipy a matrix and a vector
+//
+// IN:		m		The matrix (left hand side)
+//			v		The vector (right hand side)
+//
+// RETURN:	[m]*v
+TVECTOR Matrix_Vector_Multiply(TMATRIX m, TVECTOR v)
+{
+	// TODO LAB 2: Replace with your implementation.
+	return v;
+}
+
+// Multipy a vector and a matrix
+//
+// IN:		v		The vector ( left hand side)
+//			m		The matrix (right hand side)
+//
+// RETURN:	v*[m]
+TVECTOR Vector_Matrix_Multiply(TVECTOR v, TMATRIX m)
+{
+	// TODO LAB 2: Replace with your implementation.
+	return v;
+}
+// Multiply a matrix by a matrix
+//
+// IN:		m		First Matrix (left hand side)
+//			n		Second Matrix (right hand side)
+//
+// RETURN:	[m]*[n]
+TMATRIX Matrix_Matrix_Multiply(TMATRIX m, TMATRIX n)
+{
+	// TODO LAB 2: Replace with your implementation.
+	return m;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Matrix Functions Lab # 3
+///////////////////////////////////////////////////////////////////////
+
+// HELPER FUNCTION  *** NOT GRADED, ONLY SUGGESTED ***
+// USE THIS FUNCTION TO FIND THE DETERMINANT OF A 3*3
+// MATRIX. IT CAN BE USED IN THE MATRIX DETERMINANT
+// AND MATRIX INVERSE FUNCTIONS BELOW
+// 
+// RETURN:	The determinant of a 3x3 matrix
+float Matrix_Determinant(float e_11,float e_12,float e_13,
+						 float e_21,float e_22,float e_23,
+						 float e_31,float e_32,float e_33)
+{
+	return 0;
+}
+
+// Get the determinant of a matrix
+//
+// IN:		m		The ONE!
+//
+// RETURN:	It's deterinant
+float Matrix_Determinant(TMATRIX m)
+{
+	// TODO LAB 3: Replace with your implementation.
+	return 0;
+}
+
+// Get the inverse of a matrix
+//
+// IN:		m		The matrix to inverse
+//
+// RETURN:	The Inverse of [m]
+//
+// NOTE: Returns the matrix itself if m is not invertable.
+TMATRIX Matrix_Inverse(TMATRIX m)
+{
+	// TODO LAB 3: Replace with your implementation.
+	return m;
+}
+
